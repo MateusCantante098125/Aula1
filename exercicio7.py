1507071c0e3db61a27259d181066f6a39f8a376b
Mateus Cantante <2004mateus@gmail.com>
2025-09-17 17:22:43 +0100

    .

--- exercicio7.py
+++ exercicio7.py
@@ -1,9 +1,10 @@
-def alg_maior():
-    numero = (input("Digite um número: "))
+def alg_maior(numero):
+    
     algarismo_maior = "0"
     for i in numero:
         if i > algarismo_maior:
             algarismo_maior = i
     print("O maior algarismo é:", algarismo_maior)
     
-alg_maior()
+a = (input("Digite um número: "))
+alg_maior(a)
