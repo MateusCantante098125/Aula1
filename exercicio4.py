cb582754e298d9df2295657cf8e11fedbcc5dcb1
Mateus Cantante <2004mateus@gmail.com>
2025-09-18 11:51:52 +0100

    Exercício4

---
+++ exercicio4.py
@@ -0,0 +1,8 @@
+valor = float(input("Introduza um valor de raio: "))
+pi = 3.14
+
+perimetro = 2*pi * valor
+area = pi * valor**2
+
+print("O perímetro da circunferência é: ", perimetro)
+print("A área do circulo é: ", area)
