79481974a9c860f2b75268a3baf25c892c85ff58
Mateus Cantante <2004mateus@gmail.com>
2025-09-20 14:42:47 +0100

    Exercício 2

---
+++ exercício2.py
@@ -0,0 +1,7 @@
+x1 = int(input("Introduza o valor de x do ponto 1: "))
+y1 = int(input("Introduza o valor de y do ponto 1: "))
+x2 = int(input("Introduza o valor de x do ponto 2: "))
+y2 = int(input("Introduza o valor de y do ponto 2: "))
+
+distancia = ((x2-x1)**2 + (y2-y1)**2)**0.5
+print("A distância entre os pontos é:", distancia)
