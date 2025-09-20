2de6ef8b29f48c412122326eade645228caf63e4
Mateus Cantante <2004mateus@gmail.com>
2025-09-20 15:08:34 +0100

    Exercício 3

---
+++ exercício3.py
@@ -0,0 +1,26 @@
+while True: 
+    nota_matematica = float(input("Introduza a nota de Matemática (0-20): "))
+    if 0 <= nota_matematica <= 20:
+        break
+    
+while True:
+    nota_portugues = float(input("Introduza a nota de Português (0-20): "))
+    if 0 <= nota_portugues <= 20:
+        break
+    
+while True:
+    nota_ingles = float(input("Introduza a nota de Inglês (0-20): "))
+    if 0 <= nota_ingles <= 20:
+        break
+    
+while True:
+    nota_geografia = float(input("Introduza a nota de Geografia (0-20): "))
+    if 0 <= nota_geografia <= 20:
+        break
+
+media = (nota_matematica + nota_portugues + nota_ingles + nota_geografia) / 4
+
+if media >= 9.5:
+    print("Aprovado", media, "valores")
+else:
+    print("Reprovado")
