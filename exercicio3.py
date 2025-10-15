bbff7438d1ea1c6d0a550f84d4114866a83cbdba
Mateus Cantante <2004mateus@gmail.com>
2025-10-15 19:43:07 +0100

    Exercício 3

---
+++ exercicio3.py
@@ -0,0 +1,19 @@
+lista = []
+menores = []
+n_elementos = int(input("Qual o tamanho da lista? "))
+
+for i in range(n_elementos+1):
+    aux = int(input("Digite um número: "))
+    lista.append(aux)
+
+valor = int(input("Digite um número para buscar na lista: "))
+
+for numero in lista:
+    if numero == valor:
+        print(f"O número {valor} foi encontrado na lista.")
+        var = 1
+        break
+  
+if var != 1:
+    print(f"O número {valor} não foi encontrado na lista.")
+                    
